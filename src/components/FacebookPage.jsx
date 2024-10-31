d6737d8130aefb0676510c36dfcbe56947ccc423
Tani1964 <ifegbesan6@gmail.com>
2024-10-31 20:36:10 +0100

    added new accounts

--- src/components/FacebookPage.jsx
+++ src/components/FacebookPage.jsx
@@ -1,18 +1,41 @@
-import { Box, Text, Image, Flex } from "@chakra-ui/react";
+import { useState } from "react";
+import { Box, Text, Spinner } from "@chakra-ui/react";
 
 function FacebookPage() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleIframeLoad = () => {
+    setIsLoading(false);
+  };
+
   return (
-    <Box p={8} maxW="1200px" mx="auto">
+    <Box p={8} maxW="1200px" mx="auto" position="relative">
       <Text fontSize="2xl" fontWeight="bold" mb={4}>
         Our Facebook Page
       </Text>
+
+      {/* Loader */}
+      {isLoading && (
+        <Box
+          position="absolute"
+          top="50%"
+          left="50%"
+          transform="translate(-50%, -50%)"
+          zIndex="1"
+        >
+          <Spinner size="xl" color="blue.500" />
+        </Box>
+      )}
+
+      {/* Iframe */}
       <iframe
         src="https://widgets.sociablekit.com/facebook-page-posts/iframe/25481832"
         frameBorder="0"
         width="100%"
         height="1000"
-        style={{ border: "none" }} // Optional: remove default border
+        style={{ border: "none", display: isLoading ? "none" : "block" }}
         title="Facebook Page Posts"
+        onLoad={handleIframeLoad}
       ></iframe>
     </Box>
   );
--- src/components/Mixlr.jsx
+++ src/components/Mixlr.jsx
@@ -1,18 +1,41 @@
-import { Box, Text, Image, Flex } from "@chakra-ui/react";
+import { useState } from "react";
+import { Box, Text, Spinner } from "@chakra-ui/react";
 
 function Mixlr() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleIframeLoad = () => {
+    setIsLoading(false);
+  };
+
   return (
-    <Box p={8} maxW="1200px" mx="auto">
+    <Box p={8} maxW="1200px" mx="auto" position="relative">
       <Text fontSize="2xl" fontWeight="bold" mb={4}>
-      Our Mixlr page
+        Our Mixlr page
       </Text>
+
+      {/* Loader */}
+      {isLoading && (
+        <Box
+          position="absolute"
+          top="50%"
+          left="50%"
+          transform="translate(-50%, -50%)"
+          zIndex="1"
+        >
+          <Spinner size="xl" color="blue.500" />
+        </Box>
+      )}
+
+      {/* Iframe */}
       <iframe
         src="https://nbbcil.mixlr.com/"
         frameBorder="0"
         width="100%"
         height="1000"
-        style={{ border: "none" }} // Optional: remove default border
+        style={{ border: "none", display: isLoading ? "none" : "block" }}
         title="Our Mixlr page"
+        onLoad={handleIframeLoad}
       ></iframe>
     </Box>
   );
