29a6c730c1d5cbccdacdd1b6e7da7446ef4933de
TANITOLUWA IFEGBESAN <101012325+Tani1964@users.noreply.github.com>
2024-11-02 12:19:50 +0000

    text formatting

--- src/components/Who.jsx
+++ src/components/Who.jsx
@@ -19,7 +19,7 @@ const Who = () => {
         <Box
           p={{ base: 4, md: 8 }} // Responsive padding
           width={{ base: "100%", md: "50%" }} // Full width on small screens, half on medium+
-          textAlign={{ base: "center", md: "left" }} // Center text on small screens, left-align on larger
+          textAlign={{ base: "left", md: "left" }} // Center text on small screens, left-align on larger
         >
           <Heading mb={4} fontSize={{ base: "2xl", md: "4xl" }}>Who are we...</Heading>
           <Text fontSize={{ base: "sm", md: "md" }}>
@@ -31,7 +31,7 @@ const Who = () => {
           </Text>
         </Box>
       </Flex>
-      <Box p={{ base: 4, md: 16 }}>
+      <Box p={{ base: 8, md: 16 }}>
         <Text fontSize={{ base: "sm", md: "md" }}>
           As a church, we provide a warm, welcoming, and inspiring atmosphere for Christian worship, teaching, and community. We are committed to proclaiming the truth of God's word, equipping believers to live out their faith, and raising up leaders who will continue this important work. Through dynamic preaching, engaging small groups, and Spirit-led ministries, we aim to see lives transformed by the power of the gospel, producing fruit that will last for eternity.
           <br /><br />
