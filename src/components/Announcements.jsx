f2e149b102df352d68470df3bb8ea47c501b070b
TANITOLUWA IFEGBESAN <101012325+Tani1964@users.noreply.github.com>
2024-12-02 18:58:03 +0000

    december update

--- src/components/Announcements.jsx
+++ src/components/Announcements.jsx
@@ -20,16 +20,12 @@ const Announcement = () => {
         alignItems="center" // Center align items
         animation="scroll 30s linear infinite"
       >
-        <Text as="span" color="white" mx="1" lineHeight="1.2">|| 1-3: Let My Gate Be Opened – Continuous Praise. Psalm 34:1 – 5:30 pm daily</Text>
-        <Text as="span" color="white" mx="1" lineHeight="1.2">|| 3: Church Building Dedication & Annual Thanksgiving – Testimonies of God’s Wonders. Psalm 126:3 – 9am</Text>
-        <Text as="span" color="white" mx="1" lineHeight="1.2">|| 9: Alone With God – 10am</Text>
-        <Text as="span" color="white" mx="1" lineHeight="1.2">|| 10: Singles & Useful Fellowship – Contact Peace – 12 Noon</Text>
-        <Text as="span" color="white" mx="1" lineHeight="1.2">|| 15: Intercessory Night – Night Of Recovery. Gen 14:16 – 5:30 pm</Text>
-        <Text as="span" color="white" mx="1" lineHeight="1.2">|| 16: Evangel Bi-Annual Session – @ First Baptist Church Aguda – 9am</Text>
-        <Text as="span" color="white" mx="1" lineHeight="1.2">|| 17: MMU & WMU Meeting – 12noon</Text>
-        <Text as="span" color="white" mx="1" lineHeight="1.2">|| 23: Radiant Couple – Contact the Coordinator – 12noon</Text>
-        <Text as="span" color="white" mx="1" lineHeight="1.2">|| 24: Group Evangelism – 8:30am</Text>
-        <Text as="span" color="white" mx="1" lineHeight="1.2">|| 29-30: Community Evangelism – 5:30am/8am</Text>
+        <Text as="span" color="white" mx="1" lineHeight="1.2">|| Cross Over Night - Tuesday 31st Dec. - 10:00pm - Theme: Strategic crossing </Text>
+        <Text as="span" color="white" mx="1" lineHeight="1.2">|| Christmas Service - 25th Dec. - 08:30am - What Are You Seeking? - Text: Matthew 2:2 </Text>
+        <Text as="span" color="white" mx="1" lineHeight="1.2">|| Christmas Carol - Sunday 22nd Dec. 08:30am </Text>
+        <Text as="span" color="white" mx="1" lineHeight="1.2">|| Family Meeting - 15th Dec. 08:30am </Text>
+        <Text as="span" color="white" mx="1" lineHeight="1.2">|| Unveil 2025 (Fasting and Prayer) - 8th - 14th Dec. 05:30pm - Theme: Lift Up Your Heads - Text: Psalm 24:7 </Text>
+        <Text as="span" color="white" mx="1" lineHeight="1.2">|| Singles & Useful Fellowship - 8th Dec. - 12 noon </Text>
       </Box>
 
       <style jsx>{`
